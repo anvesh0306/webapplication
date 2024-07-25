46ef82dccc5acfe86f78805c65c20d7e76868df5
anvesh0306 <anveshaus2020@gmail.com>
2024-07-25 10:09:40 +1000

    deleated

--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7006e86f94a5a5a9d4210cc5c8671b8945f61ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6eee042ceb45036239ae5ad78b6f23be20b430d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
