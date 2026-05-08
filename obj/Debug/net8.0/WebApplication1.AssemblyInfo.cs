ddfd8d3749085e1f9c27e635aff48fad31df0001
anvesh0306 <anveshaus2020@gmail.com>
2026-05-08 13:42:30 +1000

    changed html page to revert

--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6eee042ceb45036239ae5ad78b6f23be20b430d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa36836c38fa0e8bdaf9304a575bec8961e3c970")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
